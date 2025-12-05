bb004cbc80e4381b9fdc8e14c446848a52d17431
Yashwanth-Dev18 <yashwanthdev2005@gmail.com>
2025-12-05 20:30:32 +0100

    improved version of Day1 Puzzle 2

--- D1P2.py
+++ D1P2.py
@@ -1,39 +1,22 @@
 def password():
-  num = 50
-  zero_count = 0
-  with open('D1_decoy_input.txt') as f:
-    for line in f:
-      pointer = line.strip()
-
-
-      if pointer[0] == 'L':
-        num_start = num
-        num = num - int(pointer[1:])
-        
-        distance_moved = num_start - num
-        if distance_moved < 0:
-          distance_moved = distance_moved * -1
-        zero_count += distance_moved // 100
-
-        num = num % 100
-      
-      elif pointer[0] == 'R':
-        num_start = num
-        num = num + int(pointer[1:])
-
-        distance_moved = num - num_start
-        if distance_moved < 0:
-          distance_moved = distance_moved * -1
-        zero_count += distance_moved // 100
-
-        num = num % 100
-      
-      print(num)
-      print(zero_count)
-      print("-------")
-
+    num = 50
+    zero_count = 0
     
-  return zero_count
-# veki veki song
+    with open('D1_input.txt') as f:
+        for line in f:
+            pointer = line.strip()
+            distance = int(pointer[1:])
+
+            if pointer[0] == 'L':
+                pos = (100 - num) % 100
+                zero_count += (pos + distance) // 100
+                num = (num - distance) % 100
+      
+            elif pointer[0] == 'R':
+                zero_count += (num + distance) // 100
+                num = (num + distance) % 100
+      
+
+    return zero_count
 
 print(password())
