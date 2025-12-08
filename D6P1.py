c08c167e50b93f13ddab578738862f2499f29108
Yashwanth-Dev18 <yashwanthdev2005@gmail.com>
2025-12-08 11:25:14 +0100

    successfully implemented Compactor part1 puzzle of Day 6

--- D6P1.py
+++ D6P1.py
@@ -1,4 +1,27 @@
 def compactor():
-  with open("D6_input.csv") as f:
+  data = []
+  with open("D6_input.txt") as f:
     for line in f:
-      data = line
+      items_lst = line.strip().split(" ")
+
+      temp_lst = []
+      for i in items_lst:
+        if i != "" and i != "+" and i != "*":
+          temp_lst.append(int(i))
+        elif i == "+" or i == "*":
+          temp_lst.append(i)
+        
+      data += [temp_lst]
+
+  total = 0
+  for i in range(len(data[4])):
+    if data[4][i] == "+":
+      total += data[0][i] + data[1][i] + data[2][i] + data[3][i]
+    elif data[4][i] == "*":
+      total += (data[0][i] * data[1][i] * data[2][i] * data[3][i])
+
+
+  return total
+
+
+print(compactor())
